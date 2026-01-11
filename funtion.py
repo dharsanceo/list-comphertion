02b230be052a967ec0cfe964c0fed6425205c9be
dharsanceo <sdharsan13032014@gmail.com>
2026-01-11 15:34:21 +0530

    Update funtion.py

--- funtion.py
+++ funtion.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-even_list = [n for n in numbers if n % 2 == 0]
-odd_list = [n for n in numbers if n % 2 != 0]
-print("Even numbers:", even_list)
-print("Odd numbers:", odd_list)
+nums = [1, 2, 3, 4, 5]
+even = [n for n in nums if n % 2 == 0]
+odd  = [n for n in nums if n % 2 != 0]
+print(even)
+print(odd)
+
